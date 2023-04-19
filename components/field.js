e1509c92355e9d49a362ae805ff48ba0a41e68c6
DavidAyalaDW <luis.zanabria2@unmsm.edu.pe>
2023-04-19 14:49:51 -0500

    add arrow

--- components/field.js
+++ components/field.js
@@ -76,23 +76,32 @@ export default function Field(props) {
 
         {
           type === "select" && (
-            <select
-              {...register(id)}
-              name={id}
-              id={id}
-              placeholder={placeholder}
-              className={`
-            outline-none bg-transparent border-[.5px] border-[#EAEBEF]
-            w-full block py-3 md:py-4 vw:py-[.8333vw] px-4 md:px-5 vw:px-[1.0416vw] opacity-[.85] text-lg md:text-[20px]  vw:text-[1.0416vw] leading-[22px] vw:leading-[1.1] font-normal
-                placeholder:text-[#EAEBEF]
-                `}
-            >
-              {
-                options.map((option, index) => (
-                  <option key={index} value={option.value} className='bg-body p-2 text-[#EAEBEF]'>{option.label}</option>
-                ))
-              }
-            </select>
+            <div className='flex flex-col w-full relative'>
+              <select
+                {...register(id)}
+                name={id}
+                id={id}
+                placeholder={placeholder}
+                className={`
+              outline-none bg-transparent border-[.5px] border-[#EAEBEF] remove-select-caret
+              w-full block py-3 md:py-4 vw:py-[.8333vw] px-4 md:px-5 vw:px-[1.0416vw] opacity-[.85] text-lg md:text-[20px]  vw:text-[1.0416vw] leading-[22px] vw:leading-[1.1] font-normal
+                  placeholder:text-[#EAEBEF]
+                  `}
+              >
+                {
+                  options.map((option, index) => (
+                    <option key={index} value={option.value} className='bg-body p-2 text-[#EAEBEF]'>{option.label}</option>
+                  ))
+                }
+              </select>
+
+              <div className="absolute w-6 h-6 top-[calc(50%-12px)] right-5 pointer-events-none">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                  <polyline points="6 9 12 15 18 9" />
+                </svg>
+              </div>
+            </div>
           )
         }
 
