2d21b3023a93cd165fedc01ea1e7811b11c8218a
Austen Morgan <austen@boundaryla.com>
2023-04-25 17:59:31 -0700

    Add gift card section

---
+++ components/location-card.js
@@ -0,0 +1,30 @@
+import SanityImage from './sanity-image';
+
+export default function LocationCard(props) {
+  const { location, children } = props;
+
+  return (
+    <div className="flex flex-col space-y-6">
+      <div className="flex w-full aspect-h-1 aspect-w-[1.324] md:aspect-w-[2.687] lg:aspect-w-[1.349] 3xl:aspect-w-[1.837]">
+        <div>
+          <div className="w-full h-full relative">
+            <SanityImage
+              alt={location.title}
+              src={location.image}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col space-y-4">
+        <h2 className="text-2xl md2:text-[32px]">
+          {location.title}
+        </h2>
+
+        {children}
+      </div>
+    </div>
+  );
+}
---
+++ components/sections/giftCardList.js
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import SimpleBlockContent from '../simple-block-content';
+import LocationCard from '../location-card';
+
+export default function GiftCards(props) {
+  const { title, description, locations } = props;
+
+  return (
+    <div>
+      <div className="px-6 md:px-0 md:max-w-[94.4%] 3xl:max-w-[95%] w-full mx-auto flex flex-col pt-20 md2:pt-[162px] pb-6 md2:pb-[100px]">
+        {title ? (
+          <h1 className="font-light text-center text-[32px] md2:text-[48px]">
+            {title}
+          </h1>
+        ) : null}
+
+        <div className="max-w-[500px] mx-auto text-center text-lg">
+          {description ? <SimpleBlockContent blocks={description} /> : null}
+        </div>
+      </div>
+
+      <div className="px-6 md:px-0 md:max-w-[94.4%] 3xl:max-w-[95%] w-full mx-auto flex flex-col">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-16 lg:gap-y-0 lg:gap-x-4 w-full">
+          {locations?.map((location) => (
+            <LocationCard key={location._id} location={location.location}>
+              <Link href={location.giftCardUrl}>
+                <a className="opacity-80 tracking-[.05em] uppercase text-lg leading-[25px] font-light underline pr-6 pb-4">
+                  Purchase Gift Card
+                </a>
+              </Link>
+            </LocationCard>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
