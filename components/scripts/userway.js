f9362f046b38dff2b3ec72c8d0f00b74c86cdcaa
Austen Morgan <austen@boundaryla.com>
2023-05-09 14:51:19 -0700

    Add GTM & UserWay scripts

---
+++ components/scripts/userway.js
@@ -0,0 +1,32 @@
+import Script from 'next/script';
+
+export default function Userway(props) {
+  const { id } = props;
+
+  if (!id) {
+    return null;
+  }
+
+  return (
+    <>
+      <Script id="userway">
+        {`
+          (function (d) {
+            var s = d.createElement('script');
+            s.setAttribute('data-account', '${id}');
+            s.setAttribute('src', 'https://cdn.userway.org/widget.js');
+            (d.body || d.head).appendChild(s);
+          })(document);
+        `}
+      </Script>
+
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `    
+            <noscript>Please ensure Javascript is enabled for purposes of <a href="https://userway.org">website accessibility</a></noscript>
+          `,
+        }}
+      />
+    </>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
-import Head from 'next/head'
-import '../styles/globals.scss'
+import Head from 'next/head';
+import '../styles/globals.scss';
+import GTM from '@/components/scripts/gtm';
+import Userway from '@/components/scripts/userway';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <GTM id={process.env.NEXT_PUBLIC_GTM_ID} />
+      <Userway id={process.env.NEXT_PUBLIC_USERWAY_ID} />
       <Head>
         <link rel="shortcut icon" href="/images/Sparrow Favicon.png" />
       </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
