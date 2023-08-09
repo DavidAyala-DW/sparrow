0f60bb813955d8f5bf397b6138c8adb467bcecf6
Austen Morgan <austen@boundaryla.com>
2023-08-09 13:59:01 -0700

    Add inkind script

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import GTM from '@/components/scripts/gtm'
 import Userway from '@/components/scripts/userway'
 import '../styles/globals.css'
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <GTM id={process.env.NEXT_PUBLIC_GTM_ID} />
       <Userway id={process.env.NEXT_PUBLIC_USERWAY_ID} />
+      <Script src="https://inkindscript.com/inkind.js" />
       <Head>
         <link rel="shortcut icon" href="/images/Sparrow Favicon.png" />
       </Head>
